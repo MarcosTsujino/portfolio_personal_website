dfedbfb2ed221f711cb7355d8473bfa96de716c7
MarcosTsujino <75284215+MarcosTsujino@users.noreply.github.com>
2024-09-23 00:26:53 -0300

    Update index.jsx

--- src/components/pages/Sobre/index.jsx
+++ src/components/pages/Sobre/index.jsx
@@ -8,13 +8,15 @@ function Sobre() {
                 <div className={styles.texto}>
                     <h2>Sobre</h2>   
                     <p>
-                        Olá! Meu nome é Marcos Yoshio Nakao Tsujino, sou Engenheiro de Controle e Automação, em transição de carreira para o desenvolvimento web. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas no Centro Universitário FMU, com previsão de formatura para dezembro de 2024.
-
+                        Olá! Meu nome é Marcos, sou Engenheiro de Controle e Automação, em transição de carreira para o desenvolvimento web. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas no Centro Universitário FMU, com previsão de formatura para dezembro de 2024.
+                    </p>
+                    <p>
 Minha trajetória profissional começou na área de eletrônica, onde adquiri uma sólida experiência como Técnico em Eletrônica na I/O Eletrônica. Durante esse período, desenvolvi habilidades em manutenção de equipamentos eletrônicos, montagem de placas de circuito impresso e resolução de problemas técnicos. A partir dessa base, comecei a expandir meus conhecimentos em desenvolvimento web, me especializando em tecnologias como HTML5, CSS3, JavaScript, React.js e Python 3.
-
-Tenho uma paixão por aprender novas tecnologias e criar soluções inovadoras. Além de ser certificado em ferramentas como AutoCAD 2D e 3D, SolidWorks, Power BI e Excel, também sou fluente em inglês, o que me permite colaborar com projetos internacionais.
-
-Busco oportunidades para aplicar meus conhecimentos e crescer na área de desenvolvimento front-end, utilizando minhas habilidades técnicas para criar experiências digitais intuitivas e eficientes.
+</p>
+<p>
+                    Tenho uma paixão por aprender novas tecnologias e criar soluções inovadoras. Além de ser certificado em ferramentas como AutoCAD 2D e 3D, SolidWorks, Power BI e Excel, também sou fluente em inglês, o que me permite colaborar com projetos internacionais.
+</p>
+<p>Busco oportunidades para aplicar meus conhecimentos e crescer na área de desenvolvimento front-end, utilizando minhas habilidades técnicas para criar experiências digitais intuitivas e eficientes.
                     </p>
                     
                 </div> 
