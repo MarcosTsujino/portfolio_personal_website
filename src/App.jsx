28434bf4bbb157cf80e4bbea51b8de971b54571a
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2024-09-22 22:54:19 -0300

    "Updated index.css: removed Google Fonts import, changed font family, added padding to universal selector, and replaced body styles with root variables and colors."

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import React from "react";
 import './App.css'
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Skills from "./components/Skills/Skills";
-import WorkExperience from "./components/WorkExperience/WorkExperience";
-import ContactMe from "./components/ContactMe/ContactMe";
-import Footer from "./components/Footer/Footer";
+import AppRoutes from './routes'
 
-const App = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <Hero />
-                <Skills />
-                <WorkExperience />
-                <ContactMe />
-            </div>
-            <Footer />
-        </>
-    );
-};
+function App () {
+  return (
+    <AppRoutes />
+  )
+}
 
-export default App;
+export default App
